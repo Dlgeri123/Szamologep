cd31ea15e3d0166db74d753ea0ba04e881ab154a
Dlgeri123 <43840813+Dlgeri123@users.noreply.github.com>
2018-10-24 21:07:58 +0200

    Felület kijavítása

--- src/szamologep/Szamologep.java
+++ src/szamologep/Szamologep.java
@@ -18,20 +18,21 @@ import javafx.stage.Stage;
 public class Szamologep extends Application {
     
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws Exception {/*
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
-        stage.show();
+        stage.show();*/
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        Main m = new Main();
+        m.show();
     }
     
 }
